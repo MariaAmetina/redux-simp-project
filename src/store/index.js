babd71130d08acbb3491301ba76e65a96e23474e
MariiaAmetina <ametina.ms@gmail.com>
2021-12-13 21:42:44 +0300

    added actions

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,35 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
+    //state.counter++; // НЕЛЬЗЯ ИЗМЕНЯТЬ СУЩЕСТВУЮЩИЙ СТЕЙТ, ВСЕГДА НУЖНО ПЕРЕПИСЫВАТЬ ЕГО return-ОМ
     return {
+      //нужно указывать каждый раз все поля initialState, потому что в редюсере они не merge-тся, а заменяются на новые
       counter: state.counter + 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "increase") {
+    return {
+      counter: state.counter + action.increaseAmount,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === "decrement") {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if ((action.type = "toggle")) {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
     };
   }
 
